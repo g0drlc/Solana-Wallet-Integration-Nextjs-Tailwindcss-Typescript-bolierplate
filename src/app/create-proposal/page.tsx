d1124f3dad2e8c86e4165348fa68fe7bc819efe3
g0drlc <utommy.abs@gmail.com>
2025-03-02 06:43:07 +0900

    fix: build error

--- src/app/create-proposal/page.tsx
+++ src/app/create-proposal/page.tsx
@@ -1,9 +1,5 @@
 "use client"
-import ConnectButton from "@/components/ConnectButton";
 import CreateMarket from "@/components/proposal/CreateProposal";
-import ProposalPage from "@/components/proposal/ProposalPage";
-import ViewProposal from "@/components/proposal/ViewProposal";
-import Image from "next/image";
 
 export default function CreateProposal() {
   return (
--- src/app/view-proposals/page.tsx
+++ src/app/view-proposals/page.tsx
@@ -1,15 +1,9 @@
 "use client"
-import ConnectButton from "@/components/ConnectButton"
-import ProposalPage from "@/components/proposal/ProposalPage"
 import ViewProposal from "@/components/proposal/ViewProposal"
 import React from "react"
 
-export default function CreateProposal({
-    children,
-}: {
-    children: React.ReactNode
-}) {
+export default function CreateProposal() {
     return (
-            <ViewProposal />
+        <ViewProposal />
     )
 }
